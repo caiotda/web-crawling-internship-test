0f9ab9f38777a257bfc00020b4599b86222a840c
Caio Andrade <caiotdandrade@gmail.com>
2021-07-13 16:08:27 -0300

    feat: includes only refrigerators on sale
    - modifies url in order to include a filter. The website
      accepts a parameter on the requisition called "des".
      "des" describes the range the product has. I've set it as 0TO25
      to include every discount. It could be set to 0 to 100, i believe.
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- crawler.py
+++ crawler.py
@@ -4,8 +4,7 @@ import requests
 def get_html(url):
     return requests.get(url).text
 
-refrigerators_url = 'https://www.extra.com.br/c/eletrodomesticos/refrigeradores\
-                    /?filtro=c13_c14_C13'
+refrigerators_url = 'https://www.extra.com.br/c/eletrodomesticos/refrigeradores/?filtro=c13_c14_c13&des=0TO25'
 
 tvs_url = 'https://www.extra.com.br/c/tv-e-video/televisores/\
           ?filtro=c1_c2&nid=202485'
