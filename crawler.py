6760bbdd96b2fdf6980b376bc8d577606d36b830
Caio Andrade <caiotdandrade@gmail.com>
2021-07-13 10:51:16 -0300

    chore: bootstraps crawler with target urls

---
+++ crawler.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_html(url):
+    return requests.get(url).text
+
+refrigerators_url = 'https://www.extra.com.br/c/eletrodomesticos/refrigeradores\
+                    /?filtro=c13_c14_C13'
+
+tvs_url = 'https://www.extra.com.br/c/tv-e-video/televisores/\
+          ?filtro=c1_c2&nid=202485'
+
+printers_url = 'https://www.extra.com.br/c/informatica/z\
+                impressoras/?filtro=c56_c61'
